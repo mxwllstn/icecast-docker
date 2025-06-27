d3b4c5fc014635e876a65253e5bf9eb630f0e72e
Max Stein <maxwell.stein@gmail.com>
2025-06-27 15:10:50 -0700

    feat: adds more variables to start script

--- start.sh
+++ start.sh
@@ -5,19 +5,26 @@ env
 set -x
 
 set_val() {
-    if [ -n "$2" ]; then
-        echo "set '$2' to '$1'"
-        sed -i "s/<$2>[^<]*<\/$2>/<$2>$1<\/$2>/g" /etc/icecast2/icecast.xml
+    value="$1"
+    tag="$2"
+
+    if [ -n "$tag" ] && [ -n "$value" ]; then
+        echo "set '$tag' to '$value'"
+        sed -i "s|<${tag}>[^<]*</${tag}>|<${tag}>${value}</${tag}>|g" /etc/icecast2/icecast.xml
     else
-        echo "Setting for '$1' is missing, skipping." >&2
+        echo "Setting for '$value' is missing, skipping." >&2
     fi
 }
 
-set_val $ICECAST_SOURCE_PASSWORD source-password
-set_val $ICECAST_RELAY_PASSWORD  relay-password
-set_val $ICECAST_ADMIN_PASSWORD  admin-password
-set_val $ICECAST_PASSWORD        password
-set_val $ICECAST_HOSTNAME        hostname
+set_val "$ICECAST_SOURCE_PASSWORD" source-password
+set_val "$ICECAST_RELAY_PASSWORD"  relay-password
+set_val "$ICECAST_ADMIN_PASSWORD"  admin-password
+set_val "$ICECAST_PASSWORD"        password
+set_val "$ICECAST_HOSTNAME"        hostname
+set_val "$ICECAST_LOCATION"        location
+set_val "$ICECAST_ADMIN_EMAIL"     admin
+set_val "$ICECAST_MAX_CLIENTS"     clients
+set_val "$ICECAST_MAX_SOURCES"     sources
 
 set -e
 
